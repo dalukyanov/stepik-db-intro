7e901deeb080be1d5afae914d57e77eaba9af61c
Dmitry Lukyanov <40296063+dalukyanov@users.noreply.github.com>
2019-06-03 13:59:13 +0300

    Решённые задачи в разделе 1.2 Базовые операции SQL

---
+++ 1 Введение и базовые операции SQL/task_1_2_2_simple_insert.sql
@@ -0,0 +1,8 @@
+insert into billing values (
+    'pasha@mail.com',
+    'katya@mail.com',
+    300.00,
+    'EUR',
+    '2016-02-14',
+    'Valentines day present)'
+);
---
+++ 1 Введение и базовые операции SQL/task_1_2_3_simple_update.sql
@@ -0,0 +1,3 @@
+update billing
+	set payer_email='igor@mail.com'
+where payer_email = 'alex@mail.com';
